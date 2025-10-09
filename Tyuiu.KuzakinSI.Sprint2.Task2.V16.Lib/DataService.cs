4de8efcb63be1af702358577cc3927477c053300
KuzakinSemyon <kuzakinsemen@gmail.com>
2025-10-09 20:26:27 +0500

    Task2

--- Tyuiu.KuzakinSI.Sprint2.Task2.V16.Lib/DataService.cs
+++ Tyuiu.KuzakinSI.Sprint2.Task2.V16.Lib/DataService.cs
@@ -10,8 +10,8 @@ namespace Tyuiu.KuzakinSI.Sprint2.Task2.V16.Lib
             bool inLeftBlock = (x >= 3 && x <= 7) && (y >= 3 && y <= 10);
             bool inRightBlock = (x >= 12 && x <= 14) && (y >= 3 && y <= 8);
             bool inBottomBlock = (x >= 3 && x <= 7) && (y >= 11 && y <= 13);
-            bool inMiddleConnector = (x >= 6 && x <= 10) && (y >= 7 && y <= 9);
-            
+            bool inMiddleConnector = (x >= 6 && x <= 10) && (y >= 6 && y <= 9);
+
             return inLeftBlock || inRightBlock || inBottomBlock || inMiddleConnector;
         }
     }
